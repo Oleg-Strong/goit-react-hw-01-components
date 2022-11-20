67de27ed6745fd60ff95c40aaaa92bb104c59bd9
Oleg-Strong <superstrong0@gmail.com>
2022-11-20 19:32:03 +0200

    create goit-react-hw-01-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+
+import { Profile } from "components/Profile/Profile"
+import user from "../cunfigs/user"
+import { Statistics } from "components/Statistics/Statistics"
+import data from "../cunfigs/data"
+import { FriendList } from "components/FriendList/FriendList"
+import friends from "../cunfigs/friends"
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory"
+import transactions from "../cunfigs/transactions" 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        padding: "20px",
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+      />
+      <Statistics
+        title='Upload stats'
+        stats={data}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
